ee90d34bafba4178a7aaefac68156b1888403aa5
tigi8 <vardani.tigran@gmail.com>
2024-11-10 23:27:03 +0300

    Update index.js

--- auth-microfrontend/index.js
+++ auth-microfrontend/index.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './components/Login';
+import Register from './components/Register';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <div>
+      <h1>Auth Microfrontend</h1>
+      <Login />
+      <Register />
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
