af39336feee6553971279771383e520045dc0f81
tigi8 <vardani.tigran@gmail.com>
2024-11-10 23:26:37 +0300

    Update webpack.config.js

--- auth-microfrontend/webpack.config.js
+++ auth-microfrontend/webpack.config.js
@@ -1,1 +1,47 @@
+const path = require('path');
+const { ModuleFederationPlugin } = require('webpack').container;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    port: 3001,
+  },
+  output: {
+    publicPath: 'http://localhost:3001/',
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'auth',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Login': './src/components/Login',
+        './Register': './src/components/Register',
+      },
+      shared: {
+        react: { singleton: true, eager: true, requiredVersion: '^17.0.2' },
+        'react-dom': { singleton: true, eager: true, requiredVersion: '^17.0.2' },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
