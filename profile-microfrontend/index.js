47aaa555ee87019b194d5239d1d59ee3982187ef
tigi8 <vardani.tigran@gmail.com>
2024-11-11 00:09:26 +0300

    Update index.js

--- profile-microfrontend/index.js
+++ profile-microfrontend/index.js
@@ -1,1 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Profile from "./components/Profile"; // Импорт основного компонента профиля
+import "./styles/profile.css"; // Импорт стилей профиля (если есть)
 
+const App = () => (
+  <div>
+    <h1>Profile Microfrontend</h1>
+    <Profile /> {/* Рендеринг компонента профиля */}
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
