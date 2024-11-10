8f96ca312378c74cc1698fd944ddb09f7f828a9d
tigi8 <vardani.tigran@gmail.com>
2024-11-11 00:12:19 +0300

    Update index.js

--- gallery-microfrontend/index.js
+++ gallery-microfrontend/index.js
@@ -1,1 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Gallery from "./components/Gallery"; // Импорт основного компонента галереи
+import "./styles/gallery.css"; // Импорт стилей для галереи
 
+const App = () => (
+  <div>
+    <h1>Gallery Microfrontend</h1>
+    <Gallery /> {/* Рендеринг компонента галереи */}
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
